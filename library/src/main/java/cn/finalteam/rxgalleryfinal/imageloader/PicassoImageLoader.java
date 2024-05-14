a8f45d4ccce99611a0c43ce76088d4c11d3442b2
Coffee <iamsunnycoffee@gmail.com>
2024-05-14 17:06:33 +0800

    1.1.3.b 修改Picasso版本

--- library/src/main/java/cn/finalteam/rxgalleryfinal/imageloader/PicassoImageLoader.java
+++ library/src/main/java/cn/finalteam/rxgalleryfinal/imageloader/PicassoImageLoader.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 
-import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.RequestCreator;
 
@@ -22,12 +21,11 @@ public class PicassoImageLoader implements AbsImageLoader {
 
     @Override
     public void displayImage(Context context, String path, FixImageView imageView, Drawable defaultDrawable, Bitmap.Config config, boolean resize, boolean isGif, int width, int height, int rotate) {
-        RequestCreator creator = Picasso.get()
+        RequestCreator creator = Picasso.with(context)
                 .load(new File(path))
                 .placeholder(defaultDrawable)
                 .error(defaultDrawable)
                 .rotate(rotate)
-                .networkPolicy(NetworkPolicy.NO_STORE)
                 .config(config)
                 .tag(context);
         if (resize) {
