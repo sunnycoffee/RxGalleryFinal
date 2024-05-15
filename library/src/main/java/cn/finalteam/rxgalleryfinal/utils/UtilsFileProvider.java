01403a6efee0d817ef3e711a8cb786280f8def32
Coffee <iamsunnycoffee@gmail.com>
2024-05-15 17:17:39 +0800

    1.1.3.d 处理FileProvider重名问题

---
+++ library/src/main/java/cn/finalteam/rxgalleryfinal/utils/UtilsFileProvider.java
@@ -0,0 +1,9 @@
+package cn.finalteam.rxgalleryfinal.utils;
+
+import androidx.core.content.FileProvider;
+
+/**
+ * @author sunnycoffee
+ */
+public class UtilsFileProvider extends FileProvider {
+}
